3ca7b510f257e11e850ad54cf09d4919cbf896db
student <student@Admins-MacBook-Pro.local>
2024-12-05 16:26:05 -0500

    Found and loaded dataset and defined menu constants.

---
+++ README.md
@@ -0,0 +1,4 @@
+Pseudocode:
+Print Main Menu and options
+Take user input and store in variable
+If user selects 1, then print user choice and print options
--- main.py
+++ main.py
@@ -1,17 +1,42 @@
 # Enter description here
-
-
-
+# 
+# 
+#
+#
 # Add imports here
 import pandas as pd
 import matplotlib as plt
-c
+
 # Put constants and configuration here
+def main_menu():
+    print(
+        "*-Water Potability Data Analyzer-*\n"
+    "1. View Potability Analysis.\n" 
+    "2. View Explanation of Different Data Categories.\n" 
+    "3. Create Data Visualization.\n"
+    "4. Generate Solutions Report.\n"
+    )
 
+def potability_analysis():
+    print(
+        "1. Simplified Analysis."
+        "2. Verbose Analysis."
+        "3. Identify Trends in Data"
+        )
 
+def data_category_explanation():
+    print(
+        "1. "
+    )
+
+def data_visualization():
+    print(
+        "1. Display Overall Data Visualizations"
+    )
 
 # Add data processing functions here
-
+def load_dataset():
+    df = pd.read_csv("water_potability.csv")
 
 
 # Add analysis functions here 
@@ -28,3 +53,5 @@ c
 
 
 # Add main program functions here
+
+main_menu()
